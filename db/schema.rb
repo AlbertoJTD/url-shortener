2bfa6ec86cd6c33d5ae1cd7a083b6a4b1ae4d0f2
AlbertoJTD <albertojtd@outlook.com>
2025-11-09 22:39:58 -0600

    Add link model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_10_043712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string "url"
+    t.string "title"
+    t.string "description"
+    t.string "image"
+    t.integer "views_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
