6abc14eafee66887382ac5d122813a6e537ede34
AlbertoJTD <albertojtd@outlook.com>
2025-11-10 08:31:40 -0600

    Add decode method to ShortCode

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -4,6 +4,7 @@ class ShortCode
 
   def self.encode(number)
     raise ArgumentError, 'Number must be an integer' unless number.is_a?(Integer)
+    raise ArgumentError, 'Number must be positive' if number < 0
 
     # Returns `0`
     return ALPHABET[0] if number.zero? || number.nil?
@@ -20,5 +21,14 @@ class ShortCode
   end
 
   def self.decode(string)
+    raise ArgumentError, 'Decoded value must be a string' unless string.is_a?(String)
+
+    result = 0
+    string.chars.reverse.each_with_index do |character, power|
+      character_index = ALPHABET.index(character)
+      result += character_index * (BASE**power)
+    end
+
+    result
   end
 end
--- test/models/short_code_test.rb
+++ test/models/short_code_test.rb
@@ -32,4 +32,36 @@ class ShortCodeTest < ActiveSupport::TestCase
   test 'encode nil' do
     assert_raises(ArgumentError) { ShortCode.encode(nil) }
   end
+
+  test 'encode negative number' do
+    assert_raises(ArgumentError) { ShortCode.encode(-1) }
+  end
+
+  # It goes from 0 to 9
+  10.times do |i|
+    test "decode #{i}" do
+      assert_equal i, ShortCode.decode(i.to_s)
+    end
+  end
+
+  test "decode 'a'" do
+    assert_equal 10, ShortCode.decode('a')
+  end
+
+  test "decode '10'" do
+    assert_equal 62, ShortCode.decode('10')
+  end
+
+  test "decode 'gw'" do
+    assert_equal 1024, ShortCode.decode('gw')
+  end
+
+  test "decode '4c91'" do
+    assert_equal 999_999, ShortCode.decode('4c91')
+  end
+
+  test 'decode non-string' do
+    assert_raises(ArgumentError) { ShortCode.decode(10) }
+    assert_raises(ArgumentError) { ShortCode.decode(10.5) }
+  end
 end
