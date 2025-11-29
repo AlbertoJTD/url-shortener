ef155528c38987f89d6b35a5e2eaa6c5621a66ac
AlbertoJTD <albertojtd@outlook.com>
2025-11-28 21:12:25 -0600

    add view integration tests

---
+++ test/integration/view_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class ViewTest < ActionDispatch::IntegrationTest
+  test 'creates a view when a link is visited' do
+    link = links(:link_one)
+
+    assert_difference 'View.count' do
+      get view_path(link)
+
+      assert_response :redirect
+    end
+  end
+
+  test 'redirects to the link url' do
+    link = links(:link_one)
+    get view_path(link)
+
+    assert_redirected_to link.url
+  end
+end
